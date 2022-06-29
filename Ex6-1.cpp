ae85145ca32ede910a9b1dea4155cdb2265643f5
YT713538 <75293551+YT713538@users.noreply.github.com>
2022-06-29 17:48:58 +0800

    Create Ex6-1.cpp

---
+++ Ex6-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int age,age2;
+    cout<<"输入你的年龄:";
+    cin>>age;
+    cout<<"输入你朋友的年龄:";
+    cin>>age2;
+if(age>age2){
+    cout<<age<<"比较大";
+}else{
+    cout<<age2<<"比较大";
+}
+}
